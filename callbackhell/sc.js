2d1dffc3f126cf73690b7db4059c3bbc40fca5a3
jatin-05dev <jatindev214@gmail.com>
2026-07-29 18:38:37 +0530

    code

--- callbackhell/sc.js
+++ callbackhell/sc.js
@@ -21,3 +21,29 @@ const fe=()=>{
     },  2000);
 }
 fe()
+
+
+
+const fe=()=>{
+    setTimeout(() => {
+        console.log("hello");
+        setTimeout(() => {
+            console.log("puju");
+            setTimeout(() => {
+                console.log("rani");
+                setTimeout(() => {
+                    console.log("i");
+                    setTimeout(() => {
+                        console.log("love");
+                        setTimeout(() => {
+                            console.log("you");
+                            
+                        },  2000);
+                    },  2000);
+                },  2000);
+            },  2000);
+        },  2000);
+        
+    },  2000);
+}
+fe()
