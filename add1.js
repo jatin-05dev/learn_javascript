6cb94f5bd71b76471b0680941c0401c34c445a77
jatin-05dev <jatindev214@gmail.com>
2026-08-31 20:41:36 +0530

    code

--- add1.js
+++ add1.js
@@ -7,3 +7,22 @@ export function sum(a,b){
 // export {add,sum}
 // 2named
 // 3 default
+
+
+
+
+
+
+export function add(a,b){
+    return a+b
+}
+export function sum(a,b){
+    return a+b
+}
+// export {add,sum}
+// 2named
+// 3 default
+
+
+
+
