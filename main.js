001e203ae4db631be1dda40179dad82a6496c1e4
jatin-05dev <jatindev214@gmail.com>
2026-09-09 18:37:22 +0530

    code

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ console.log((ADD(5,5)))
 console.log((SUM(5,6)))
 
 
+
+
+// import sum from './add1.js'
+// console.log(sum(5,5));
+import{add as ADD ,sum as SUM} from './add1.js'
+console.log((ADD(5,5)))
+console.log((SUM(5,6)))
+
+
